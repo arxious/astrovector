148566bbd3a5c5f14166db6f845a293783ac2784
Arxious0 <16721@heathland.hounslow.sch.uk>
2026-08-29 22:51:20 +0100

    Fixed dimensions to 1280x640 Displayed an equirectangular world map; Added an icon to the program.

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,27 @@
 int main()
 {
     sf::RenderWindow window(
-        sf::VideoMode({ 800u, 600u }),
-        "Astrovector"
+        sf::VideoMode({ 1280u, 640u }),
+        "astrovector",
+		sf::Style::Titlebar | sf::Style::Close
     );
+
+    sf::Image icon;
+	if (!icon.loadFromFile("../assets/sat_icon.png"))
+	{
+		return 1;
+	}
+    window.setIcon(icon);
+
+    sf::Texture worldMap;
+	if (!worldMap.loadFromFile("../assets/blue_marble.png"))
+	{
+		return 1;
+	}
+
+	sf::Sprite worldSprite(worldMap);
+    
+	worldSprite.setPosition(sf::Vector2f(0.f, 0.f));
 
     while (window.isOpen())
     {
@@ -17,7 +35,10 @@ int main()
             }
         }
 
+
+        
         window.clear();
+        window.draw(worldSprite);
         window.display();
     }
 }
