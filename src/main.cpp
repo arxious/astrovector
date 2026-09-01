687180df4064fe849583a031c9a1937807088560
Arxious0 <16721@heathland.hounslow.sch.uk>
2026-09-01 21:35:02 +0100

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,54 @@
+#include <iostream>
+#include <array>
 #include <SFML/Graphics.hpp>
 #include "satellite.hpp"
+
+#include <libsgp4/SGP4.h>
+#include <libsgp4/Tle.h>
 
 int main()
 {
     sf::RenderWindow window(
         sf::VideoMode({ 1280u, 640u }),
         "astrovector",
-		sf::Style::Titlebar | sf::Style::Close
+        sf::Style::Titlebar | sf::Style::Close
     );
 
     sf::Image icon;
-	if (!icon.loadFromFile("../assets/sat_icon.png"))
-	{
-		return 1;
-	}
+    if (!icon.loadFromFile("../assets/sat_icon.png"))
+    {
+        return 1;
+    }
     window.setIcon(icon);
 
     sf::Texture worldMap;
-	if (!worldMap.loadFromFile("../assets/blue_marble.png"))
-	{
-		return 1;
-	}
+    if (!worldMap.loadFromFile("../assets/blue_marble.png"))
+    {
+        return 1;
+    }
 
-    sf::Clock frameClock;
-    
+    sf::Sprite worldSprite(worldMap);
 
-	sf::Sprite worldSprite(worldMap);
+    libsgp4::Tle tle("ISS (ZARYA)",
+        "1 25544U 98067A   26244.49851261  .00003910  00000-0  79223-4 0  9994",
+        "2 25544  51.6312 282.3953 0005055  96.4740 263.6825 15.48958602583586"
+    );
+
+    libsgp4::SGP4 sgp4(tle);
+
+    libsgp4::DateTime startTime = libsgp4::DateTime::Now(true);
+    libsgp4::Eci initialEci = sgp4.FindPosition(startTime);
+    libsgp4::CoordGeodetic initialGeo = initialEci.ToGeodetic();
+    libsgp4::Vector initialVel = initialEci.Velocity();
 
     SatelliteConfig testConfig;
-    testConfig.name = "testSatellite";
-    testConfig.latitude = 1280 / 2;
-    testConfig.longitude = 640 / 2;
-    testConfig.x_velocity = 100;
-    testConfig.y_velocity = 100;
+    testConfig.name = "ISS (ZARYA)";
+    testConfig.latitude = libsgp4::Util::RadiansToDegrees(initialGeo.latitude);
+    testConfig.longitude = libsgp4::Util::RadiansToDegrees(initialGeo.longitude);
+    testConfig.altitude = initialGeo.altitude;
+    testConfig.speed = initialVel.Magnitude();
 
     Satellite testSatellite(testConfig);
-    
-
-    //worldSprite.setPosition(sf::Vector2f(0, 0));
 
     while (window.isOpen())
     {
@@ -49,28 +60,35 @@ int main()
             }
         }
 
-        sf::Time elapsed = frameClock.restart();
-        float deltaTime = elapsed.asSeconds();
+        libsgp4::DateTime time = libsgp4::DateTime::Now(true);
+        libsgp4::Eci eci = sgp4.FindPosition(time);
+        libsgp4::CoordGeodetic geo = eci.ToGeodetic();
+        libsgp4::Vector vel = eci.Velocity();
 
-        testSatellite.latitude += testSatellite.x_velocity * deltaTime;
-        testSatellite.longitude -= testSatellite.y_velocity * deltaTime;
+        
+        testSatellite.latitude = libsgp4::Util::RadiansToDegrees(geo.latitude);
+        testSatellite.longitude = libsgp4::Util::RadiansToDegrees(geo.longitude);
+        testSatellite.altitude = geo.altitude;
+        testSatellite.speed = vel.Magnitude();
 
-        // couple conditions before setting pos mate
-        if (testSatellite.longitude < 0)  {
-            testSatellite.longitude = 640;
-        }
+		std::printf("Time: %s, Lat: %.6f, Lon: %.6f, Alt: %.3f km, Speed: %.3f km/s\n",
+			time.ToString().c_str(),
+			testSatellite.latitude,
+			testSatellite.longitude,
+			testSatellite.altitude,
+			testSatellite.speed
+		);
 
-        if (testSatellite.latitude < 0) {
-            testSatellite.latitude = 1280;
-        }
+        float screenX = ((testSatellite.longitude + 180.0) / 360) * 1280.0;
+        float screenY = ((90.0 - testSatellite.latitude) / 180) * 640.0;
 
-        testSatellite.visual.setPosition(sf::Vector2f(std::fmod(testSatellite.latitude, 1280), std::fmod(testSatellite.longitude, 640)));
+        testSatellite.visual.setPosition(sf::Vector2f(screenX, screenY));
 
         window.clear();
         window.draw(worldSprite);
         window.draw(testSatellite.visual);
         window.display();
     }
-}
 
-// So far what this program does is open a window of fixed dimensions and load a world map.
+    return 0;
+}
--- src/satellite.hpp
+++ src/satellite.hpp
@@ -3,42 +3,30 @@
 #include <string>
 #include <SFML/Graphics/CircleShape.hpp>
 
-struct SatelliteVisual {
-	std::string name;
-	sf::CircleShape *visual; // This will store the memory address of the circle object
-	double latitude;
-	double longitude;
-	double altitude;
-	double velocity;
-};
-
 struct SatelliteConfig {
 	std::string name;
 	double latitude;
 	double longitude;
 	double altitude;
-	double x_velocity;
-	double y_velocity;
+	double speed;
 };
 
 class Satellite {
 		
-
 	public:
 		 sf::CircleShape visual;
 		 std::string name;
 		 double latitude;
 		 double longitude;
 		 double altitude;
-		 double x_velocity;
-		 double y_velocity;
+		 double speed;
 		 Satellite(const SatelliteConfig& cfg) {
 			name = cfg.name;
 			visual = sf::CircleShape(3.0f, 100);
+			visual.setFillColor(sf::Color::Red);
 			latitude = cfg.latitude;
 			longitude = cfg.longitude;
 			altitude = cfg.altitude;
-			x_velocity = cfg.x_velocity;
-			y_velocity = cfg.y_velocity;
+			speed = cfg.speed;
 		}
 };
